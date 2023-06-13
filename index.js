111d499fa30ebcfae78f8eafdfd67565050216e6
Raphaël MANGIN <raphael.mangin6@etu.univ-lorraine.fr>
2023-06-13 12:25:22 +0200

    Etape 1

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-import photoloader from "./lib/photoloader.js";
+import {loadPicture} from "./lib/photoloader.js";
 
 
-const getPicture =
+const getPicture = function (id) {
+    loadPicture(id).then(p => {
+        console.log(p.titre);
+        console.log(p.type);
+        console.log(p.url.href);
+    });
+}
 
 getPicture(105);
 
