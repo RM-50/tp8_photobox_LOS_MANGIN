1007de541f8f1557656207b58aac383bbff67243
Raphaël MANGIN <raphael.mangin6@etu.univ-lorraine.fr>
2023-06-13 14:01:38 +0200

    Ajout methode loadResource dans photoloader ainsi que getCategory et getComments dans index.js

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-import {loadPicture} from "./lib/photoloader.js";
-
+import {loadPicture, loadResource} from "./lib/photoloader.js";
+import { prefixeURI } from "./lib/config.js";
 
 const getPicture = function (id) {
     loadPicture(id).then(p => {
-        console.log(p.titre);
-        console.log(p.type);
-        console.log(p.url.href);
+        console.log(p.photo.titre);
+        console.log(p.photo.type);
+        console.log(p.photo.url.href);
+        getCategory(p).then(cat => console.log(cat));
+        getComments(p).then(com => console.log(com));
     });
+}
+
+const getCategory = function (picture) {
+    return loadResource(picture.links.categorie.href);
+}
+
+const getComments = function (picture) {
+    return loadResource(picture.links.comments.href);
 }
 
 getPicture(105);
